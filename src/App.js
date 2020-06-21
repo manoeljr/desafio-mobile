83d171c9073ce98039ade7619f440b8146519e4d
Manoel Vieira <manoelv8@gmail.com>
2020-06-21 11:19:11 -0300

    Desafio do GoStack da RocketSeat com React Native

---
+++ src/App.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from "react";
+
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+import api from './services/api';
+
+export default function App() {
+
+  const [ repository, setRepository ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepository(response.data);
+    });
+  },[]);
+
+
+  async function handleLikeRepository(id) {
+    await api.post(`repositories/${id}/like`).then(response => {
+      const likes = response.data.likes;
+      const updateLikes = repository.filter(repository => repository.id === id ? repository.likes = likes : '');
+      setRepository([...repository, updateLikes]);
+    });
+  }
+
+  return (
+    <>
+    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+    <SafeAreaView style={styles.container}>
+
+    <FlatList data={repository} keyExtractor={repository => repository.id} renderItem={
+      ({item:repository}) => (
+      <View style={styles.repositoryContainer}>
+        <Text style={styles.repository}>{repository.title}</Text>
+        <View style={styles.techsContainer}>
+          <Text style={styles.tech}>{repository.techs}</Text>
+        </View>
+        <View style={styles.likesContainer}>
+          <Text style={styles.likeText} testID={`repository-likes-${repository.id}`}>{repository.likes} curtidas</Text>
+        </View>
+
+        <TouchableOpacity style={styles.button} onPress={() => handleLikeRepository(repository.id)} testID={`like-button-${repository.id}`}>
+          <Text style={styles.buttonText}>Curtir</Text>
+        </TouchableOpacity>
+
+      </View>
+      )
+    }/>
+
+    </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+  },
+  repositoryContainer: {
+    marginBottom: 15,
+    marginHorizontal: 15,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  repository: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  techsContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  tech: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginRight: 10,
+    backgroundColor: "#04d361",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    color: "#fff",
+  },
+  likesContainer: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  likeText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#7159c1",
+    padding: 15,
+  },
+});
